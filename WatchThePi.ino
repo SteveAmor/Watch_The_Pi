421732dc17270e98263fd892c8254e48f118e06c
ubuntu <steveamor@users.noreply.github.com>
2016-06-01 12:18:18 +0100

    LED feedback when Arduino gets a pat

--- WatchThePi.ino
+++ WatchThePi.ino
@@ -1,8 +1,13 @@
 #define RELAY 3
+#define LED 13
 
 void setup(void)
 {
   pinMode(RELAY, OUTPUT); 
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, HIGH);
+  delay(1000);
+  digitalWrite(LED, LOW);
   attachInterrupt(0, software_Reset, FALLING);
 }
 
@@ -14,7 +19,7 @@ void loop(void)
   digitalWrite(RELAY, LOW);
 }
 
-void software_Reset() // Restarts program from beginning
+void software_Reset() // Restarts program from beginning but does not reset the peripherals and registers
 {
   asm volatile ("  jmp 0");  
 }
